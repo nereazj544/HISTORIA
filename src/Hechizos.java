0dbc80cd33166fde1c9f160b79c02343d22236a2
nereazj544 <nzjvirtual@gmail.com>
2024-03-13 00:23:35 +0100

    .

---
+++ src/Hechizos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hechizos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe la palabra: ");
+
+        String p = sc.nextLine();
+        String pi = "";
+        for (int i =  p.length() -1; i >=0 ; i--) {
+            pi += p.charAt(i);
+        }
+
+        System.out.println(pi);
+    }
+}
