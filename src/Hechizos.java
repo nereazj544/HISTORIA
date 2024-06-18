c022260f1005283f698f6a7f48d0928446b161b8
nereazj544 <nzjvirtual@gmail.com>
2024-06-18 22:53:55 +0200

    f

--- src/Hechizos.java
+++ src/Hechizos.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hechizos {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
 
         System.out.println("Escribe la palabra: ");
 
         String p = sc.nextLine();
+        
+        // Invertir la palabra
         String pi = "";
-        for (int i =  p.length() -1; i >=0 ; i--) {
+        for (int i = p.length() - 1; i >= 0; i--) {
             pi += p.charAt(i);
         }
-
-        System.out.println("> Mostrar Echizo: \n" + pi);
+        
+        // Reorganizar la palabra aleatoriamente
+        char[] chars = p.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            int randomIndex = rand.nextInt(chars.length);
+            char temp = chars[i];
+            chars[i] = chars[randomIndex];
+            chars[randomIndex] = temp;
+        }
+        String randomWord = new String(chars);
+        
+        System.out.println("> Mostrar Echizo reorganizado aleatoriamente: \n" + randomWord);
+        
         sc.close();
     }
 }
